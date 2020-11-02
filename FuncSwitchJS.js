199db526aa8fdbcca9f993a10c768dbb6b4b52c5
Derek <dbacy@my.edgetechacademy.edu>
2020-11-02 10:52:19 -0600

    Initial commit

---
+++ FuncSwitchJS.js
@@ -0,0 +1,88 @@
+function convert (phrase) {
+    switch (phrase){
+        case 'A':
+            return 'Alpha';
+
+        case 'B':
+            return 'Bravo';
+
+        case 'C':
+            return 'Charlie';
+
+        case 'D':
+            return 'Delta';
+
+        case 'E':
+            return 'Echo';
+
+        case 'F':
+            return 'Foxtrot';
+
+        case 'G':
+            return 'Golf';
+
+        case 'H':
+            return 'Hotel';
+
+        case 'I':
+            return 'India';
+
+        case 'J':
+            return 'Juliett';
+
+        case 'K':
+            return 'Kilo';
+
+        case 'L':
+            return 'Lima';
+
+        case 'M':
+            return 'Mike';
+
+        case 'N':
+            return 'November';
+
+        case 'O':
+            return 'Oscar';
+
+        case 'P':
+            return 'Papa';
+
+        case 'Q':
+            return 'Quebec';
+
+        case 'R':
+            return 'Romeo';
+
+        case 'S':
+            return 'Sierra';
+
+        case 'T':
+            return 'Tango';
+
+        case 'U':
+            return 'Uniform';
+
+        case 'V':
+            return 'Victor';
+
+        case 'W':
+            return 'Whiskey';
+
+        case 'X':
+            return 'X-ray';
+
+        case 'Y':
+            return 'Yankee';
+
+        case 'Z':
+            return 'Zulu';
+
+
+        default:
+            return 'not a letter';
+    }
+    return phrase
+}
+
+
